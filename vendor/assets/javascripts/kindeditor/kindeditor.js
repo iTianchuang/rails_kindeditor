26e455c2f8b224d83110afc3321f3d56767719e5
上海添创信息技术有限公司 <itianchuang@gmail.com>
2015-02-09 09:58:54 +0800

    Update kindeditor.js

--- vendor/assets/javascripts/kindeditor/kindeditor.js
+++ vendor/assets/javascripts/kindeditor/kindeditor.js
@@ -3187,7 +3187,7 @@ _extend(KCmd, {
 	insertimage : function(url, title, width, height, border, align) {
 		title = _undef(title, '');
 		border = _undef(border, 0);
-		var html = '<img src="' + _escape(url) + '" data-ke-src="' + _escape(url) + '" ';
+		var html = '<img class="android_default" src="' + _escape(url) + '" data-ke-src="' + _escape(url) + '" ';
 		if (width) {
 			html += 'width="' + _escape(width) + '" ';
 		}
